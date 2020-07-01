637232a112f4974fce213f5ea97a17039f9cd3c2
Raghul T <raghulrage@gmail.com>
2020-07-01 10:26:44 +0530

    Updated Content

--- Data/district_download.py
+++ Data/district_download.py
@@ -5,5 +5,5 @@ def downloadData():
     with request.urlopen('https://api.covid19india.org/state_district_wise.json') as response:
         data = json.loads(response.read())
 
-    with open('district_download.json','w') as file:
+    with open('Data/district_download.json','w') as file:
         json.dump(data,file)
